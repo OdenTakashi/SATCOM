909d214d23424565201f1a6480bfdce2f4e69b2a
OdenTakashi <odentakashidesu2@gmail.com>
2026-03-14 22:34:22 +0900

    remove authentication

--- Gemfile
+++ Gemfile
@@ -11,9 +11,6 @@ gem "puma", ">= 5.0"
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
 gem "importmap-rails"
 
-# Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-gem "bcrypt", "~> 3.1.7"
-
 # LINE Bot SDK [https://github.com/line/line-bot-sdk-ruby]
 gem "line-bot-api", "~> 2.5.0"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,10 @@
 # == Route Map
 #
-#             Prefix Verb   URI Pattern                      Controller#Action
-#        new_session GET    /session/new(.:format)           sessions#new
-#       edit_session GET    /session/edit(.:format)          sessions#edit
-#            session GET    /session(.:format)               sessions#show
-#                    PATCH  /session(.:format)               sessions#update
-#                    PUT    /session(.:format)               sessions#update
-#                    DELETE /session(.:format)               sessions#destroy
-#                    POST   /session(.:format)               sessions#create
-#          passwords GET    /passwords(.:format)             passwords#index
-#                    POST   /passwords(.:format)             passwords#create
-#       new_password GET    /passwords/new(.:format)         passwords#new
-#      edit_password GET    /passwords/:token/edit(.:format) passwords#edit
-#           password GET    /passwords/:token(.:format)      passwords#show
-#                    PATCH  /passwords/:token(.:format)      passwords#update
-#                    PUT    /passwords/:token(.:format)      passwords#update
-#                    DELETE /passwords/:token(.:format)      passwords#destroy
-#           callback POST   /callback(.:format)              webhook#callback
-# rails_health_check GET    /up(.:format)                    rails/health#show
+#             Prefix Verb URI Pattern         Controller#Action
+#           callback POST /callback(.:format) webhook#callback
+# rails_health_check GET  /up(.:format)       rails/health#show
 
 Rails.application.routes.draw do
-  resource :session
-  resources :passwords, param: :token
-
   post "/callback" => "webhook#callback"
 
   get "up" => "rails/health#show", as: :rails_health_check
