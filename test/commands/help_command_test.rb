43a62dfab705cf0d31a798b97b0d1bb1c1365538
OdenTakashi <odentakashidesu2@gmail.com>
2026-03-23 17:32:39 +0900

    fix help command word

--- test/commands/help_command_test.rb
+++ test/commands/help_command_test.rb
@@ -18,7 +18,7 @@ class HelpCommandTest < ActiveSupport::TestCase
     command = HelpCommand.new(line_user_id: "U123", group_id: "G456", match_data: HelpCommand::PATTERN.match("/h"))
     result = command.call
 
-    assert_includes result, "SATCOMコマンド一覧"
+    assert_includes result, "トラウマになりそうだ"
     assert_includes result, "/金額"
     assert_includes result, "/500"
     assert_includes result, "/ … 今月の集計を表示する。"
